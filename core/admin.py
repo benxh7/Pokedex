119792093f9de2ed8ecd7eef0141efd0acb48ef1
benxh7 <banjaminmorales571@gmail.com>
2025-05-01 01:40:29 -0400

    Cambios en admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Cuenta
+from .models import Usuario
 
 class RegistroAdmin(admin.ModelAdmin):
     list_display = ('username', 'email',)
     search_fields = ('username', 'email',)
 
-admin.site.register(Cuenta, RegistroAdmin)
+admin.site.register(Usuario, RegistroAdmin)
